310b04c6011b62b66158c6c24bb6be970df8313e
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-08-10 09:26:47 +0000

    render h1 element

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <P><h1>I am learning React. My life is getting better.</h1></P>
+    </div>
   )
 }
 
